92511658849622d2830bf2412c1b8d22fe9cd75d
Muhammad Farhan <muhfarhanhidayatulloh@gmail.com>
2026-02-09 13:08:15 +0700

    feat: update Tailwind CSS integration to include JavaScript assets

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -29,7 +29,7 @@
 <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
 
 {{-- Tailwind CSS --}}
-@vite('resources/css/app.css')
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 
 {{-- Alpine.js --}}
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
