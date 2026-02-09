de119eceaef49f0e27f4374223fccfdb7747efcb
Muhammad Farhan <muhfarhanhidayatulloh@gmail.com>
2026-02-09 15:58:58 +0700

    feat: add TrustProxies middleware and configure URL scheme for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Listing;
 use App\Policies\ListingPolicy;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     {
         // Register policies
         Gate::policy(Listing::class, ListingPolicy::class);
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
